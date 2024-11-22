5dea3ab65b30b3bd3cf9ef5b3d7ee0ba7b10aa4d
Nixie <76953506+Nxyi@users.noreply.github.com>
2024-11-22 18:39:53 +0000

    QOL

--- src/main/java/net/kawaii/utils/ImUtil.java
+++ src/main/java/net/kawaii/utils/ImUtil.java
@@ -1,38 +1,28 @@
 package net.kawaii.utils;
 
 import imgui.ImGui;
-import imgui.ImGuiIO;
 import imgui.ImGuiStyle;
 import imgui.extension.implot.ImPlot;
-import imgui.flag.ImGuiConfigFlags;
 import imgui.gl3.ImGuiImplGl3;
 import imgui.glfw.ImGuiImplGlfw;
 import net.minecraft.client.MinecraftClient;
-import org.lwjgl.glfw.GLFW;
-
-import java.util.Objects;
-
-import static net.kawaii.Client.mc;
 
 public class ImUtil {
-    private static final ImGuiImplGlfw IM_GUI_IMPL_GLFW = new ImGuiImplGlfw();
-    private static final ImGuiImplGl3 IM_GUI_IMPL_GL_3 = new ImGuiImplGl3();
+    private static final ImGuiImplGlfw ImGLFW = new ImGuiImplGlfw();
+    private static final ImGuiImplGl3 ImGL3 = new ImGuiImplGl3();
 
     public static void initImGUI(){
         ImGui.createContext();
         ImPlot.createContext();
 
-        final ImGuiIO io = ImGui.getIO();
-        io.setFontGlobalScale(1f);
-
         style();
 
-        IM_GUI_IMPL_GLFW.init(MinecraftClient.getInstance().getWindow().getHandle(), true);
-        IM_GUI_IMPL_GL_3.init();
+        ImGLFW.init(MinecraftClient.getInstance().getWindow().getHandle(), true);
+        ImGL3.init();
     }
 
     public static void draw(Runnable runnable){
-        IM_GUI_IMPL_GLFW.newFrame();
+        ImGLFW.newFrame();
         ImGui.newFrame();
 
         runnable.run();
@@ -40,7 +30,7 @@ public class ImUtil {
         ImGui.endFrame();
         ImGui.render();
 
-        IM_GUI_IMPL_GL_3.renderDrawData(ImGui.getDrawData());
+        ImGL3.renderDrawData(ImGui.getDrawData());
     }
 
     public static void style(){
